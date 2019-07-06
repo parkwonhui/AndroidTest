a2cf6ec093f67720fe7a83a5748f4a272d26e593
parkwonhui <parkwonhui.dev@gmail.com>
2019-07-06 16:47:02 +0900

    android rayout 연습

--- app/src/main/java/com/example/class_4e/MainActivity.java
+++ app/src/main/java/com/example/class_4e/MainActivity.java
@@ -9,6 +9,6 @@ import android.os.Bundle;
             @Override
             protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_main);
+                setContentView(R.layout.activity_test2);
     }
 }
